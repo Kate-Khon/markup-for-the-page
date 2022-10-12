1da8eac94652f409bd01cafeb886f983df4055d2
Kate-Khon <katerynakhon@gmail.com>
2022-10-12 12:19:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="header"></header>
+      <main className="main">
+        <div className="main__picture"></div>
+        
+        <div className="main__goods">
+          <h3 className="main__subtitle">Nyheder</h3>
+          <div className="main__cards">
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+            <div className="card"></div>
+          </div>
+        </div>
+        
+        <div className="main__categories">
+          <div className="category"></div>
+          <div className="category"></div>
+          <div className="category"></div>
+          <div className="category"></div>
+          <div className="category"></div>
+          <div className="category"></div>
+        </div>
+        
+        <div className="main__gallery">
+          <h3 className="main__hashtag">#birgittebonnerup</h3>
+          <div className="main__photos">
+            <div className="photo"></div>
+            <div className="photo"></div>
+            <div className="photo"></div>
+            <div className="photo"></div>
+            <div className="photo"></div>
+            <div className="photo"></div>
+            <div className="photo"></div>
+          </div>
+        </div>
+      </main>
+      <footer className="footer"></footer>
     </div>
   );
 }
